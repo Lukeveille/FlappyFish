5d04aedd69fa73d60e22f245d505d19f8ad21f28
Lukeveille <luke@lukeleveille.com>
2019-01-20 19:30:31 -0500

    Initial commit

---
+++ Assets/Code/Fish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    [SerializeField]
+    private float _upwardForceMultiplier = 170f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool pressedFireButton = Input.GetButton("Fire1");
+        if (pressedFireButton)
+        {
+            Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector3.up * _upwardForceMultiplier);
+        }
+    }
+}
